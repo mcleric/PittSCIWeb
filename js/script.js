615c945c477f1caff2d32c46492cdb1490527069
Jim Duhala <jim.duhala@gmail.com>
2016-10-31 22:04:11 -0400

    set up navbars and footer for easy changing
    a bit more DRY now
    only using this in copy of index, index2 for now
    add top navbar with just
    <div id="navbar-top"></div>
    instead of giant copy+paste. see index2 for others (bottom navbar +
    footer)
    probably will move this to php since CS website backend uses it

--- js/script.js
+++ js/script.js
@@ -34,3 +34,10 @@ $(document).ready(function () {
         $('#wrapper').toggleClass('toggled');
   });  
 });
+
+
+$(function(){
+      $("#navbar-top").load("navbar-top.html");
+	  $("#navbar-bot").load("navbar-bot.html");
+	  $("#footer").load("footer.html");
+    });
